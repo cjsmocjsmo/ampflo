27604cea60d9cd5fdbb632fa9cc2fb6bca935baa
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-05-20 03:15:59 -0700

    added nav buttons

--- lib/playlistscreen.dart
+++ lib/playlistscreen.dart
@@ -8,6 +8,49 @@ class PlayListsScreen extends StatelessWidget {
         title: Text("Playlists"),
         backgroundColor: Colors.lightGreen[900],
       ),
+      actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              Navigator.pushNamed(
+                context,
+                '/Songs'
+              );
+            },
+            icon: Icon(Icons.album_sharp),
+            tooltip: "Go to Songs Page"
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.pushNamed(
+                context,
+                '/Playlists'
+              );
+            },
+            icon: Icon(Icons.add_rounded),
+            tooltip: "Go To Playlist Page"
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.pushNamed(
+                context,
+                '/'
+              );
+            },
+            icon: Icon(Icons.exit_to_app_sharp),
+            tooltip: "Exit App"
+          ),
+          IconButton(
+            icon: const Icon(Icons.add_alert),
+            tooltip: 'Show Snackbar',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text('ZZ Top \n Fandango \n Mexican Black Bird'),
+                backgroundColor: Colors.purple,
+              ));
+            },
+          ),
+        ]
+      ),
       body: Container(
         decoration: BoxDecoration(
           color: Colors.lightGreenAccent.shade400,
--- lib/songs.dart
+++ lib/songs.dart
@@ -8,6 +8,49 @@ class SongsScreen extends StatelessWidget {
         title: Text('Songs'),
         backgroundColor: Colors.lightGreen[900],
       ),
+      actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              Navigator.pushNamed(
+                context,
+                '/Songs'
+              );
+            },
+            icon: Icon(Icons.album_sharp),
+            tooltip: "Go to Songs Page"
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.pushNamed(
+                context,
+                '/Playlists'
+              );
+            },
+            icon: Icon(Icons.add_rounded),
+            tooltip: "Go To Playlist Page"
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.pushNamed(
+                context,
+                '/'
+              );
+            },
+            icon: Icon(Icons.exit_to_app_sharp),
+            tooltip: "Exit App"
+          ),
+          IconButton(
+            icon: const Icon(Icons.add_alert),
+            tooltip: 'Show Snackbar',
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: Text('ZZ Top \n Fandango \n Mexican Black Bird'),
+                backgroundColor: Colors.purple,
+              ));
+            },
+          ),
+        ]
+      ),
       body: Container(
         decoration: BoxDecoration(
           color: Colors.lightGreenAccent.shade400,
@@ -30,7 +73,7 @@ Widget testGridView = GridView.builder(
     mainAxisSpacing: 20,
   ),
   itemCount: 25,
-  itemBuilder: (BuildContext context) {
+  itemBuilder: (BuildContext context, index) {
     return Container(
           padding: const EdgeInsets.all(8),
           child: GestureDetector(
