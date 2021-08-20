f827d972c72e28f9f9b694f5b2a187647e82a017
Charlie Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2021-08-19 21:57:00 -0700

    movied stream builder

--- lib/screens/player2screen.dart
+++ lib/screens/player2screen.dart
@@ -93,7 +93,20 @@ class Player2Screen extends StatelessWidget {
                                       ? null
                                       : AudioService.skipToNext,
                                 ),
-                                
+                                StreamBuilder<MediaState>(
+                                  stream: _mediaStateStream,
+                                  builder: (context, snapshot) {
+                                    final mediaState = snapshot.data;
+                                    return SeekBar(
+                                      duration:
+                                          mediaState?.mediaItem?.duration ?? Duration.zero,
+                                      position: mediaState?.position ?? Duration.zero,
+                                      onChangeEnd: (newPosition) {
+                                        AudioService.seekTo(newPosition);
+                                      },
+                                    );
+                                  },
+                                ),
                               ],
                             ),
                           // if (mediaItem?.title != null) Text(mediaItem!.title),
@@ -121,20 +134,20 @@ class Player2Screen extends StatelessWidget {
                   //   },
                   // ),
                   // A seek bar.
-                  StreamBuilder<MediaState>(
-                    stream: _mediaStateStream,
-                    builder: (context, snapshot) {
-                      final mediaState = snapshot.data;
-                      return SeekBar(
-                        duration:
-                            mediaState?.mediaItem?.duration ?? Duration.zero,
-                        position: mediaState?.position ?? Duration.zero,
-                        onChangeEnd: (newPosition) {
-                          AudioService.seekTo(newPosition);
-                        },
-                      );
-                    },
-                  ),
+                  // StreamBuilder<MediaState>(
+                  //   stream: _mediaStateStream,
+                  //   builder: (context, snapshot) {
+                  //     final mediaState = snapshot.data;
+                  //     return SeekBar(
+                  //       duration:
+                  //           mediaState?.mediaItem?.duration ?? Duration.zero,
+                  //       position: mediaState?.position ?? Duration.zero,
+                  //       onChangeEnd: (newPosition) {
+                  //         AudioService.seekTo(newPosition);
+                  //       },
+                  //     );
+                  //   },
+                  // ),
                   // Image.network(mediaItem!.artUri.toString()),
                   // // Display the processing state.
                   // StreamBuilder<AudioProcessingState>(
@@ -560,53 +573,7 @@ class Sleeper {
 
 class SleeperInterruptedException {}
 
-/// A wrapper around FlutterTts that makes it easier to wait for speech to
-/// complete.
-// class Tts {
-//   final FlutterTts _flutterTts = new FlutterTts();
-//   Completer? _speechCompleter;
-//   bool _interruptRequested = false;
-//   bool _playing = false;
 
-//   Tts() {
-//     _flutterTts.setCompletionHandler(() {
-//       _speechCompleter?.complete();
-//     });
-//   }
-
-//   bool get playing => _playing;
-
-//   Future<void> speak(String text) async {
-//     _playing = true;
-//     if (!_interruptRequested) {
-//       _speechCompleter = Completer();
-//       await _flutterTts.speak(text);
-//       await _speechCompleter!.future;
-//       _speechCompleter = null;
-//     }
-//     _playing = false;
-//     if (_interruptRequested) {
-//       _interruptRequested = false;
-//       throw TtsInterruptedException();
-//     }
-//   }
-
-//   Future<void> stop() async {
-//     if (_playing) {
-//       await _flutterTts.stop();
-//       _speechCompleter?.complete();
-//     }
-//   }
-
-//   void interrupt() {
-//     if (_playing) {
-//       _interruptRequested = true;
-//       stop();
-//     }
-//   }
-// }
-
-// class TtsInterruptedException {}
 
 class Seeker {
   final AudioPlayer player;
